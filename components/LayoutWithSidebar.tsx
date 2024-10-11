74cd1e475c9b0ceba934bff0e040b48a9fcf6276
iChuck-W <164290338+iChuck-W@users.noreply.github.com>
2024-10-11 21:33:07 +0800

    update-08

--- components/LayoutWithSidebar.tsx
+++ components/LayoutWithSidebar.tsx
@@ -1,3 +1,7 @@
+// .eslintrc.json 中新增禁用规则
+// "jsx-a11y/click-events-have-key-events": "off",
+// "jsx-a11y/no-static-element-interactions": "off",
+
 'use client'
 
 import React, { useState, useEffect } from 'react'
