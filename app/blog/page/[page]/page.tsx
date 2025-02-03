531430003a2a88d6c86ff4796c14d1ef88c1911b
iChuck-W <164290338+iChuck-W@users.noreply.github.com>
2025-02-03 21:34:51 +0800

    Updated according to the original project

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -11,9 +11,9 @@ export const generateStaticParams = async () => {
   return paths
 }
 
-export default function Page({ params }: { params: { page: string } }) {
+export default async function Page({ params }: { params: Promise<{ page: string }> }) {
   const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = parseInt(params.page as string)
+  const pageNumber = parseInt((await params).page)
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
     POSTS_PER_PAGE * pageNumber
